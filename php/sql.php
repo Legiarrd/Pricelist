e1a0f7ad100bfc60a79e30d448c3c66e411f67d0
legiarrd <28425334+Legiarrd@users.noreply.github.com>
2019-02-08 15:01:27 +0100

    Delete sql.php

--- php/sql.php
+++
@@ -1,5 +0,0 @@
-<?php
-#$config = parse_ini_file("ini/db.ini");
-#$pdo =  new PDO(sprintf("mysql:host=%s;dbname=%s", $config['host'], $config['dbname']), $config['dbusername'], $config['dbpw']);
-#$pdo = new PDO("mysql:host=localhost;dbname=database", 'root', '');
- ?>
