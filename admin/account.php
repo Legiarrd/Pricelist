5ffe11c8ca215166335f25127ef53a6febd40289
legiarrd <28425334+Legiarrd@users.noreply.github.com>
2019-02-15 10:04:33 +0100

    fix html

--- admin/account.php
+++ admin/account.php
@@ -26,9 +26,9 @@ if(!isset($_SESSION['userid'])) {
           <a class="btn btn-dark" href='edit.php'>Einträge bearbeiten</a>
           <a class="btn btn-dark" href='add.php'>Eintrag hinzufügen</a>
           <a class="btn btn-dark" href='users.php'>Users</a>
-          <a class="btn btn-dark active" href='account.php'">Account bearbeiten</a>
+          <a class="btn btn-dark active" href='account.php'>Account bearbeiten</a>
         </div>
-        <div class=" collapse navbar-collapse justify-content-end">
+        <div class="collapse navbar-collapse justify-content-end">
           <ul class="navbar-nav">
           <a class="btn btn-dark" href='../index.php'><i class="fas fa-home"></i> Zur Startseite</a>
           <a class="btn navbar-btn btn-dark"href='../php/logout.php'><i class="fas fa-sign-out-alt"></i> Logout</a>
